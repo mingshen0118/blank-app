208f83971757eced1b267722670ba207ae50975b
mingshen0118 <129404655+mingshen0118@users.noreply.github.com>
2024-09-11 09:48:33 +0000

    try

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ st.write(
 st.write(
     "Let's start building! For help, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+failures = st.number_input("Number of past class failures", min_value=0, max_value=4)
+higher = st.selectbox('Wants to take higher education?', ('Yes', 'No'))
+G1= st.slider("Your First Period Grade", 0, 20, 10)
+G2= st.slider("Your Second Period Grade", 0, 20, 10)
+
+st.button("Predict Final Grade")
